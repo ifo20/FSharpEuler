8ddfc35c17557783ba19a9aab3d1f1fb9c23238b
MERIDIAN\IOlliver <iolliver@travelrepublic.co.uk>
2018-07-03 18:08:49 +0100

    Initial setup

--- FSharpEuler/src/App/Program.fs
+++ FSharpEuler/src/App/Program.fs
@@ -6,34 +6,37 @@ open Library
 [<EntryPoint>]
 let main argv =
 
-    let exists pid = 
-        (Map.containsKey pid Problem.Titles)
-        && (Map.containsKey pid Problem.Descriptions)
-        && (Map.containsKey pid Solution.solvers)
+    //let exists pid = 
+    //    (Map.containsKey pid Problems.Titles)
+    //    && (Map.containsKey pid Problems.Descriptions)
+    //    && (Map.containsKey pid Solution.solvers)
 
-    let display problemId = 
-        printfn "%A" Problem.Titles.[problemId]
-        printfn "%A" Problem.Descriptions.[problemId]
+    //let display problemId = 
+    //    printfn "%A" Problems.Titles.[problemId]
+    //    printfn "%A" Problems.Descriptions.[problemId]
 
-    let solve problemId = 
-        if exists problemId then
-            display problemId |> ignore
-            let solver = Solution.solvers.[problemId]
-            solver()
-        else
-            printfn "Don't have that yet" |> ignore
-            -1
+    //let solve problemId = 
+    //    if exists problemId then
+    //        display problemId |> ignore
+    //        let solver = Solution.solvers.[problemId]
+    //        solver()
+    //    else
+    //        printfn "Don't have that yet" |> ignore
+    //        -1
 
-    let rec requestInput () = 
-        printfn "Hello World from F#! Enter the number of the problem you wish to solve."
-        match Console.ReadLine() |> Int32.TryParse with
-        | true, c -> 
-            solve c |> ignore
-            requestInput ()
-        | false, _ -> 
-            printfn "That wasn't an integer"
-            requestInput ()
+    //let rec requestInput () = 
+    //    printfn "Hello World from F#! Enter the number of the problem you wish to solve."
+    //    match Console.ReadLine() |> Int32.TryParse with
+    //    | true, c -> 
+    //        solve c |> ignore
+    //        requestInput ()
+    //    | false, _ -> 
+    //        printfn "That wasn't an integer"
+    //        requestInput ()
 
-    requestInput() |> ignore
+    //requestInput() |> ignore
     
+    Solutions.print 1
+    Solutions.print 2
+
     0 // return an integer exit code
